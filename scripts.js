1de60bbbc485a15185a8e4dcf0c762cc0023ea79
Raufeder <you@example.com>
2020-10-08 08:33:48 -0400

    final touches

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
-const baseURL = 'http://api.airvisual.com/v2/countries?key=';
+const baseURL = 'http://api.airvisual.com/v2/';
 const key  = '8bac110c-fd5a-4b14-a6b0-f94021ccc437';
 let url;
 
-const country = document.querySelector('.country');
-const select = document.querySelector('select');
 const searchForm = document.querySelector('form');
+const countrySelect = document.getElementById('selector1');
+const stateSelect = document.getElementById('selector2');
+const citySelect = document.getElementById('selector3');
 
-let dropDown = document.addEventListener('change', fetchResults);
+let chosenState;
+let chosenCity;
 
-
-fetch(baseURL + key)
+fetch(baseURL + 'countries?key=' + key)
     .then(result => {
         return result.json(); 
     })
@@ -22,34 +23,77 @@ function countryDropDown(json) {
         let countryName = json.data[i].country;
         let option = document.createElement('option');
 
-        option.value = country;
+        option.value = countryName;
         option.innerHTML = `<span>${countryName}</span>`;
 
-        select.appendChild(option);
+        countrySelect.appendChild(option);
     }
 }
 
-function toggleState() {
-    let stateBox = document.getElementById('selector2');
-    if(stateBox.style.display == 'block') {
-        stateBox.style.display = 'none';
-    }
-    else {
-        stateBox.style.display == 'block';
+function getCity(event) {
+    fetch(baseURL + 'cities?state=' + event.target.value + '&country=' + countrySelect.value + '&key=' + key)
+        .then(result => {
+            return result.json(); 
+        })
+        .then(json => {
+            toggleCity(json); 
+        });
+}
+stateSelect.addEventListener('change', getCity);
+
+function getState(event) {
+    fetch(baseURL + 'states?country=' + event.target.value + '&key=' + key)
+        .then(result => {
+            return result.json(); 
+        })
+        .then(json => {
+            toggleState(json); 
+        });
+}
+countrySelect.addEventListener('change', getState);
+
+function toggleState(json) {
+    for(stateName of json.data) {
+        let option = document.createElement('option');
+
+        option.value = stateName.state;
+        option.innerHTML = `<span>${stateName.state}</span>`;
+
+        stateSelect.appendChild(option);
     }
 }
 
-function toggleCity() {
-    let cityBox = document.getElementById('selector2');
-    if(cityBox.style.display == 'block') {
-        cityBox.style.display = 'none';
-    }
-    else {
-        cityBox.style.display == 'block';
+function toggleCity(json) {
+    for(cityName of json.data) {
+        let option = document.createElement('option');
+
+        option.value = cityName.city;
+        option.innerHTML = `<span>${cityName.city}</span>`;
+
+        citySelect.appendChild(option);
     }
 }
-
+citySelect.addEventListener('change', fetchResults);
+//fix search history states and cities
 function fetchResults(e) {
-    
-    
+    e.preventDefault();
+    url = baseURL + 'city?city=' + citySelect.value + '&state=' + stateSelect.value + '&country=' + countrySelect.value + '&key=' + key
+
+    fetch(url).then(function(result) {
+        return result.json();
+    }).then(function(json) {
+        displayResults(json);
+        console.log(json);
+    });
+};
+
+function displayResults(json) {
+    let cityData = json.data.city; //current city
+    let currentTime = json.data.current.pollution.ts; //timeStamp
+    let polltuionIndex = json.data.current.pollution.aqicn; //AQI value based on China MEP standard
+    let tempInCelcius = json.data.current.weather.tp; //temperature in celcius
+    currentCity.innerHTML = cityData;
+    timeStamp.innerHTML = currentTime;
+    celciusTemp.innerHTML = tempInCelcius;
+    pollution.innerHTML = polltuionIndex;
 }
