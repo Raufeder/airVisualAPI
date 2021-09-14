6c62dea4bc96fff6fe860d4a07a6b26c51b324ed
Raufeder <you@example.com>
2021-09-13 23:44:56 -0400

    debugging pt 1

--- public/scripts.js
+++ public/scripts.js
@@ -1,106 +1,130 @@
-const baseURL = 'https://api.airvisual.com/v2/';
-const key  = '8bac110c-fd5a-4b14-a6b0-f94021ccc437';
+const baseURL = "https://api.airvisual.com/v2/";
+const key = "8bac110c-fd5a-4b14-a6b0-f94021ccc437";
 let url;
-
-const searchForm = document.querySelector('form');
-const countrySelect = document.getElementById('selector1');
-const stateSelect = document.getElementById('selector2');
-const citySelect = document.getElementById('selector3');
+//TODO Make it just USA
+//TODO Temperature in Fairenheit
+//TODO Clear population selection before clicking new option
+const searchForm = document.querySelector("form");
+const countrySelect = document.getElementById("selector1");
+const stateSelect = document.getElementById("selector2");
+const citySelect = document.getElementById("selector3");
 
 let chosenState;
 let chosenCity;
 
-fetch(baseURL + 'countries?key=' + key)
-    .then(result => {
-        return result.json(); 
-    })
-    .then(json => {
-        countryDropDown(json); 
-    });
+fetch(baseURL + "countries?key=" + key)
+  .then((result) => {
+    return result.json();
+  })
+  .then((json) => {
+    countryDropDown(json);
+    console.log(json);
+    console.log(json.data);
+  });
 //COUNTRY DROPDOWN FUNCTION
 function countryDropDown(json) {
-    for (let i = 0; i < json.data.length; i++) {
-        let countryName = json.data[i].country;
-        let option = document.createElement('option');
+  for (let i = 0; i < json.data.length; i++) {
+    let countryName = json.data[i].country;
+    let option = document.createElement("option");
 
-        option.value = countryName;
-        option.innerHTML = `<span>${countryName}</span>`;
+    option.value = countryName;
+    option.innerHTML = `<span>${countryName}</span>`;
 
-        countrySelect.appendChild(option);
-    }
+    countrySelect.appendChild(option);
+  }
 }
 
 //STATE DROPDOWN FUNCTION
 function toggleState(json) {
-    for(stateName of json.data) {
-        let option = document.createElement('option');
+  for (stateName of json.data) {
+    let option = document.createElement("option");
 
-        option.value = stateName.state;
-        option.innerHTML = `<span>${stateName.state}</span>`;
+    option.value = stateName.state;
+    option.innerHTML = `<span>${stateName.state}</span>`;
 
-        stateSelect.appendChild(option);
-    }
+    stateSelect.appendChild(option);
+  }
 }
 
 //CITY DROPDOWN FUNCTION
 function toggleCity(json) {
-    for(cityName of json.data) {
-        let option = document.createElement('option');
+  for (cityName of json.data) {
+    let option = document.createElement("option");
 
-        option.value = cityName.city;
-        option.innerHTML = `<span>${cityName.city}</span>`;
+    option.value = cityName.city;
+    option.innerHTML = `<span>${cityName.city}</span>`;
 
-        citySelect.appendChild(option);
-    }
+    citySelect.appendChild(option);
+  }
 }
 
 //CHOSEN COUNTRY TO POPULATE STATES
 function getState(event) {
-    fetch(baseURL + 'states?country=' + event.target.value + '&key=' + key)
-        .then(result => {
-            return result.json(); 
-        })
-        .then(json => {
-            toggleState(json); 
-        });
+  fetch(baseURL + "states?country=" + event.target.value + "&key=" + key)
+    .then((result) => {
+      return result.json();
+    })
+    .then((json) => {
+      toggleState(json);
+      console.log(json.data);
+    });
 }
-countrySelect.addEventListener('change', getState);
+countrySelect.addEventListener("change", getState);
 
 //CHOSEN STATE TO POPULATE CITIES
 function getCity(event) {
-    fetch(baseURL + 'cities?state=' + event.target.value + '&country=' + countrySelect.value + '&key=' + key)
-        .then(result => {
-            return result.json(); 
-        })
-        .then(json => {
-            toggleCity(json); 
-        });
+  fetch(
+    baseURL +
+      "cities?state=" +
+      event.target.value +
+      "&country=" +
+      countrySelect.value +
+      "&key=" +
+      key
+  )
+    .then((result) => {
+      return result.json();
+    })
+    .then((json) => {
+      toggleCity(json);
+      console.log(json.data);
+    });
 }
-stateSelect.addEventListener('change', getCity);
+stateSelect.addEventListener("change", getCity);
 
-countrySelect.addEventListener('change', fetchResults);
-citySelect.addEventListener('change', fetchResults);
-citySelect.addEventListener('change', fetchResults);
+// countrySelect.addEventListener("change", fetchResults);
+citySelect.addEventListener("change", fetchResults);
+// stateSelect.addEventListener("change", fetchResults);
 
 function fetchResults(e) {
-    e.preventDefault();
-    url = baseURL + 'city?city=' + citySelect.value + '&state=' + stateSelect.value + '&country=' + countrySelect.value + '&key=' + key
+  e.preventDefault();
+  url =
+    baseURL +
+    "city?city=" +
+    citySelect.value +
+    "&state=" +
+    stateSelect.value +
+    "&country=" +
+    countrySelect.value +
+    "&key=" +
+    key;
 
-    fetch(url).then(function(result) {
-        return result.json();
-    }).then(function(json) {
-        displayResults(json);
-
+  fetch(url)
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (json) {
+      displayResults(json);
     });
-};
+}
 
 function displayResults(json) {
-    let cityData = json.data.city; //current city
-    let currentTime = json.data.current.pollution.ts; //timeStamp
-    let polltuionIndex = json.data.current.pollution.aqicn; //AQI value based on China MEP standard
-    let tempInCelcius = json.data.current.weather.tp; //temperature in celcius
-    currentCity.innerHTML = cityData;
-    timeStamp.innerHTML = currentTime;
-    celciusTemp.innerHTML = tempInCelcius + '°C';
-    pollution.innerHTML = polltuionIndex;
+  let cityData = json.data.city; //current city
+  let currentTime = json.data.current.pollution.ts; //timeStamp
+  let polltuionIndex = json.data.current.pollution.aqicn; //AQI value based on China MEP standard
+  let tempInCelcius = json.data.current.weather.tp; //temperature in celcius
+  currentCity.innerHTML = cityData;
+  timeStamp.innerHTML = currentTime;
+  celciusTemp.innerHTML = tempInCelcius + "°C";
+  pollution.innerHTML = polltuionIndex;
 }
